2b3d5f484fab312bcd7d32adafbaf99b7248841a
Meet Sheth <meet.sheth@bacancy.com>
2020-12-21 16:38:04 +0530

    stashing applied

--- test.js
+++ test.js
@@ -3,3 +3,4 @@ console.log('File edited after creating second_branch');
 
 console.log('1st commit after stashing');
 console.log('2nd commit affter stashing');
+console.log('New changes that are going to be stashed');
