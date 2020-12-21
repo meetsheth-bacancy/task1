ae2ba47c9bf576ad653cfd42cb60b921d4a5fd4f
Meet Sheth <meet.sheth@bacancy.com>
2020-12-21 16:33:02 +0530

    2nd commit after stashing

--- test.js
+++ test.js
@@ -2,3 +2,4 @@ console.log('File made on local comp');
 console.log('File edited after creating second_branch');
 
 console.log('1st commit after stashing');
+console.log('2nd commit affter stashing');
